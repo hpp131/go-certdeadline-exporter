a17f45d90cef797747346ec2eb11762715961bba
han <han@handeMacBook-Air.local>
2022-12-06 15:42:55 +0800

    costom collector  version0.0.1

--- sslcert-exporter.go
+++ sslcert-exporter.go
@@ -9,18 +9,26 @@ import (
 	"time"
 )
 
-func main() {
-	//checkDomain := flag.String("domain", "", "a set of domains to check cert's deadline")
-	deadline_gauge := prometheus.NewGaugeFunc(prometheus.GaugeOpts{
-		Name:      "ssl_cert_deadline",
-		Namespace: "default",
-		Help:      "computer the distence to expire_time of ssl_cert",
-	}, checkHttps)
-	prometheus.MustRegister(deadline_gauge)
-	http.Handle("/metrics", promhttp.Handler())
-	fmt.Println("already start tcp listen")
+type sslStateCollector struct {
+	expireDay *prometheus.Desc
+}
 
-	http.ListenAndServe(":9999", nil)
+func NewSslStateCollector(domain string) prometheus.Collector {
+	return &sslStateCollector{
+		expireDay: prometheus.NewDesc(
+			"deadline",
+			"the deadline of specific domain",
+			nil,
+			prometheus.Labels{"domain_name": domain})}
+}
+
+func (s *sslStateCollector) Describe(ch chan<- *prometheus.Desc) {
+	ch <- s.expireDay
+}
+
+func (s *sslStateCollector) Collect(ch chan<- prometheus.Metric) {
+
+	ch <- prometheus.MustNewConstMetric(s.expireDay, prometheus.GaugeValue, checkHttps())
 }
 
 func checkHttps() (t float64) {
@@ -39,4 +47,23 @@ func checkHttps() (t float64) {
 	fmt.Println("过期时间", certinfo.NotAfter)
 	return certinfo.NotAfter.Sub(time.Now()).Hours() / 24
 
+}
+
+func main() {
+	//checkDomain := *flag.String("domain", "", "a set of domains to check cert's deadline")
+	//
+	//deadline_gauge := prometheus.NewGaugeFunc(prometheus.GaugeOpts{
+	//	Name:      "ssl_cert_deadline",
+	//	Namespace: "default",
+	//	Help:      "computer the distence to expire_time of ssl_cert",
+	//}, checkHttps)
+	//prometheus.MustRegister(deadline_gauge)
+	//http.Handle("/metrics", promhttp.Handler())
+	//fmt.Println("already start tcp listen")
+	//
+	//http.ListenAndServe(":9999", nil)
+	prometheus.MustRegister(NewSslStateCollector("www.baidu.com"))
+	//reg.Register(NewSslStateCollector("https://www.badiu.com"))
+	http.Handle("/metrics", promhttp.Handler())
+	http.ListenAndServe(":9999", nil)
 }
