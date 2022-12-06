90b923e7adcc9c447d4126a0ae714fa7cbca6262
han <han@192.168.0.101>
2022-12-06 21:32:01 +0800

    support check multiple domain by using flag

--- sslcert-exporter.go
+++ sslcert-exporter.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"crypto/tls"
+	"flag"
 	"fmt"
+	//"github.com/prometheus/client_golang/api/prometheus/"
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"net/http"
+	"strings"
 	"time"
 )
 
@@ -13,45 +16,61 @@ type sslStateCollector struct {
 	expireDay *prometheus.Desc
 }
 
-func NewSslStateCollector(domain string) prometheus.Collector {
+func NewSslStateCollector() prometheus.Collector {
 	return &sslStateCollector{
 		expireDay: prometheus.NewDesc(
 			"deadline",
 			"the deadline of specific domain",
-			nil,
-			prometheus.Labels{"domain_name": domain})}
+			[]string{"domain_name"},
+			nil)}
 }
 
 func (s *sslStateCollector) Describe(ch chan<- *prometheus.Desc) {
 	ch <- s.expireDay
 }
 
+/*
+use map to store domain: deadline
+*/
 func (s *sslStateCollector) Collect(ch chan<- prometheus.Metric) {
-
-	ch <- prometheus.MustNewConstMetric(s.expireDay, prometheus.GaugeValue, checkHttps())
+	for key, value := range checkHttps() {
+		ch <- prometheus.MustNewConstMetric(s.expireDay, prometheus.GaugeValue, value, key)
+	}
+	//ch <- prometheus.MustNewConstMetric(s.expireDay, prometheus.GaugeValue, checkHttps(), "www.baidu.com")
 }
 
-func checkHttps() (t float64) {
-	tr := &http.Transport{
-		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+func checkHttps() (t map[string]float64) {
+	/*
+		get domain string and split into []string
+	*/
+	domainString := *flag.String("domains", "", "a set of domain separated by comma")
+	domainSlice := strings.Split(domainString, ",")
+	var deadlineMap map[string]float64
+	for _, value := range domainSlice {
+		checkUrl := "https://" + value
+		tr := &http.Transport{
+			TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+		}
+		client := &http.Client{Transport: tr}
+		resp, err := client.Get(checkUrl)
+		fmt.Printf("the panic is happend")
+		defer resp.Body.Close() ///// panic
+		if err != nil {
+			fmt.Errorf("requeset is failed", err)
+		}
+		certinfo := resp.TLS.PeerCertificates[0]
+		fmt.Println("过期时间", certinfo.NotAfter)
+		//deadlineSlice = append(deadlineSlice, certinfo.NotAfter.Sub(time.Now()).Hours()/24)
+		deadlineMap[value] = certinfo.NotAfter.Sub(time.Now()).Hours() / 24
 	}
-	client := &http.Client{Transport: tr}
-	checkUrl := "https://www.baidu.com"
-	resp, err := client.Get(checkUrl)
-	fmt.Printf("the panic is happend")
-	defer resp.Body.Close() ///// panic
-	if err != nil {
-		fmt.Errorf("requeset is failed", err)
-	}
-	certinfo := resp.TLS.PeerCertificates[0]
-	fmt.Println("过期时间", certinfo.NotAfter)
-	return certinfo.NotAfter.Sub(time.Now()).Hours() / 24
+
+	return deadlineMap
 
 }
 
 func main() {
 	reg := prometheus.NewRegistry()
-	reg.Register(NewSslStateCollector("www.baidu.com"))
+	reg.Register(NewSslStateCollector())
 	http.Handle("/metrics", promhttp.HandlerFor(reg, promhttp.HandlerOpts{Registry: reg}))
 	http.ListenAndServe(":9999", nil)
 }
